4126bb409753b76a3a36ca75eda831ee2fa21b77
nlee1025git <nlee1025.githubber@gmail.com>
2023-12-01 19:41:50 -0500

    add

---
+++ test/java/org/openjfx/BusinessObjectTest.java
@@ -0,0 +1,45 @@
+package org.openjfx;
+
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This function is in charge of testing the methods of the BusinessObject Class.
+ * @author Nam Kyu Lee.
+ */
+class BusinessObjectTest {
+    String petitionerLastName = "Bell";
+    String petitionerFirstName = "Alan";
+    String businessName = "Soft Ware";
+    String address = "123 Main St, Fairfax, VA 22032";
+    String phoneNumber = "7031234567"; // 703-123-4567
+    String email = "software@email.com";
+    String EIN = "123456777"; // 12-3456777
+
+    String beneficiaryLastName = "Baker";
+    String beneficiaryFirstName = "Jane";
+    String beneficiaryDOB = "10201985"; // 10/20/1985
+    String beneficiaryGender = "F";
+    String beneficiaryA_Number = "123456789"; // 123-456-789
+    String beneficiaryCountryOfBirth = "Canada";
+    String beneficiaryAddress = "456 Lee Hwy, Toronto, ON M9C 1W3";
+    String beneficiaryArrivalDate = "11202023"; // 11/20/2023
+    String beneficiaryPassportExpirationDate = "07152028"; // 07/15/2028
+
+    String workID = "123456";
+    //Step currentStep = Step.DATA_ENTRY;
+    boolean approveStatus = false;
+    boolean emailSent = false;
+
+    BusinessObject bo = new BusinessObject(petitionerLastName, petitionerFirstName, businessName, address, phoneNumber, email, EIN,
+                                           beneficiaryLastName, beneficiaryFirstName, beneficiaryDOB, beneficiaryGender, beneficiaryA_Number,
+                                           beneficiaryCountryOfBirth, beneficiaryAddress, beneficiaryArrivalDate, beneficiaryPassportExpirationDate, workID);
+
+    /**
+     * Test 1 tests getPetitionerLastName() method
+     */
+    @Test
+    public void testGetPetitionerLastName() {
+        assertEquals("This should return petitioner's last name.", petitionerLastName, bo.getPetitionerLastName());
+    }
+}
