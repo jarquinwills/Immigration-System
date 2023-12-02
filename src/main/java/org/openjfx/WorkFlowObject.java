b636eaf2d989d36b60d640546dd7640ed4cd4f13
nlee1025git <99083497+nlee1025git@users.noreply.github.com>
2023-12-01 21:49:36 -0500

    Update WorkFlowObject.java

--- src/main/java/org/openjfx/WorkFlowObject.java
+++ src/main/java/org/openjfx/WorkFlowObject.java
@@ -6,6 +6,24 @@ package org.openjfx;
  * @author Wills Jarquin
  *
  */
+
+import java.util.LinkedList;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class WorkFlowObject extends Application{
 	
 	String id;
